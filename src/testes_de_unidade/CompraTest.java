960a2bde82aca14d064d9ab68f4369c3c6a73e72
amanda-magalhaes <amanda.magalhaes@ccc.ufcg.edu.br>
2018-08-01 21:39:15 -0300

    Testes de Unidade!

--- src/testes_de_unidade/CompraTest.java
+++ src/testes_de_unidade/CompraTest.java
@@ -13,13 +13,6 @@ import entidades.Compra;
 
 class CompraTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-		//ItemQI item1 = new ItemQI("Pepero Biscoito de Palito de Chocolate", "alimento industrializado", 1, 6, "4");
-		//ItemUnidade item2 = new ItemUnidade("toalha de banho", "higiene pessoal", 2, 2);
-		//ItemQuilo item3 = new ItemQuilo("limao", "alimento nao industrializado", 2.0, 3);
-	}
-
 	@Test
 	void testCompra() {
 		ItemQuilo item3 = new ItemQuilo("limao", "alimento nao industrializado", 2.0, 3);
@@ -29,4 +22,44 @@ class CompraTest {
 		Compra compra2 = new Compra(2, item2);
 		assertEquals(2, compra2.getQuantidade());
 	}
+	
+	@Test
+	void testAdicionaQuantidade() {
+		ItemUnidade item2 = new ItemUnidade("toalha de banho", "higiene pessoal", 2, 2);
+		Compra compra2 = new Compra(10, item2);
+		assertEquals(10, compra2.getQuantidade());
+		compra2.adicionaQuantidade(3);
+		assertEquals(13, compra2.getQuantidade());
+	}
+	
+	@Test
+	void testDiminuiQuantidade() {
+		ItemQI item1 = new ItemQI("Pepero Biscoito de Palito de Chocolate", "alimento industrializado", 1, 6, "4");
+		Compra compra3 = new Compra(3, item1);
+		compra3.diminuiQuantidade(2);
+		assertEquals(1, compra3.getQuantidade());
+	}
+	
+	@Test
+	void testAlterarQuantidade() {
+		ItemQI item1 = new ItemQI("Pepero Biscoito de Palito de Chocolate", "alimento industrializado", 1, 6, "4");
+		Compra compra4 = new Compra(2, item1);
+		compra4.setQuantidade(5);
+		assertEquals(5, compra4.getQuantidade());
+	}
+	
+	@Test
+	void testGetItem() {
+		ItemQuilo item3 = new ItemQuilo("limao taiti", "alimento nao industrializado", 2.0, 3);
+		Compra compra = new Compra(5, item3);
+		assertEquals("3. limao taiti, alimento nao industrializado, Preco por quilo: <>", compra.getItem());
+	}
+	
+	@Test
+	void testToString() {
+		ItemQuilo item3 = new ItemQuilo("limao", "alimento nao industrializado", 2.0, 3);
+		Compra compra1 = new Compra(5, item3);
+		assertEquals("5 limao, alimento nao industrializado", compra1.toString());
+	}
+	
 }
